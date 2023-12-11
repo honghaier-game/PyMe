7f770fd51520208170265f1049c79666547c648b
Honghaier <59077757+honghaier-game@users.noreply.github.com>
2023-12-11 11:47:33 +0800

    PyMe v1.3.7.3
    Update to Version 1.3.7.3

---
+++ PyMe/_internal/cv2/version.py
@@ -0,0 +1,5 @@
+opencv_version = "4.8.1.78"
+contrib = False
+headless = False
+rolling = False
+ci_build = True
--- PyMe/cv2/version.py
+++
@@ -1,5 +0,0 @@
-opencv_version = "4.8.0.76"
-contrib = True
-headless = False
-rolling = False
-ci_build = True
